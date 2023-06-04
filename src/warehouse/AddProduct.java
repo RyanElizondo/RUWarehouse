d6f17ce971e01d59b8d36857861aa01a1da5b29e
Ryan Elizondo-Fallas <ryanelizondo10@gmail.com>
2023-06-03 20:51:13 -0400

    Adding RUWarehouse

---
+++ src/warehouse/AddProduct.java
@@ -0,0 +1,17 @@
+package warehouse;
+
+/*
+ * Use this class to test to addProduct method.
+ */
+public class AddProduct {
+    public static void main(String[] args) {
+    /*
+        StdIn.setFile(args[0]);
+        StdOut.setFile(args[1]);
+       */
+        System.out.print("hi");
+
+
+	// Use this file to test addProduct
+    }
+}
